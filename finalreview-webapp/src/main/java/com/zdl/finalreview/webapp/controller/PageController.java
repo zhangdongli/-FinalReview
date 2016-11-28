e247cba45dcbde6f7cbe372567f227723585a4c0
dongli.zhang <zhangdongli@ubtour.com>
2016-11-28 08:51:06 +0800

    spring mvc 搭建完毕

---
+++ finalreview-webapp/src/main/java/com/zdl/finalreview/webapp/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.zdl.finalreview.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by zhangdongli on 16-11-28.
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String getHome(){
+        return "home";
+    }
+}
---
+++ finalreview-webapp/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhangdongli
+  Date: 16-11-28
+  Time: 上午8:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>我的网页</h1>
+<hr>
+<div>
+        <span>
+            欢迎访问
+        </span>
+</div>
+</body>
+</html>
